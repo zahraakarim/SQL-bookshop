b34033c400a85ab1f1f25079a3f60e33c0ace45b
Paul Rogerson <paul.rogerson@northcoders.com>
2021-11-19 16:53:07 +0000

    Specify to modify the table, not update

--- db/18-handle-delete-parent-record.sql
+++ db/18-handle-delete-parent-record.sql
@@ -1,14 +1,14 @@
 \c my_bookshop
 
 -- Deleting a book:
--- Update the books_genres table here:
+-- Modify the books_genres table here:
 
 -- Delete a book here:
 
 
 -- Deleting an author:
--- Update the books table here:
+-- Modify the books table here:
 
--- Update the books_genres table here:
+-- Modify the books_genres table here:
 
 -- Delete an author here:
