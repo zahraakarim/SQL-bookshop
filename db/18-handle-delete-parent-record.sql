2efa423f641437697c3a7dff895762984f33dbb9
P-Copley <31252326+P-Copley@users.noreply.github.com>
2022-03-18 16:33:45 +0000

    match comments to instructions

--- db/18-handle-delete-parent-record.sql
+++ db/18-handle-delete-parent-record.sql
@@ -1,14 +1,14 @@
 \c my_bookshop
 
 -- Deleting a book:
--- Modify the books_genres table here:
+-- Modify the books_genres table in section 14 first
 
 -- Delete a book here:
 
 
 -- Deleting an author:
--- Modify the books table here:
+-- Modify the books table in section 02 first
 
--- Modify the books_genres table here:
+-- Modify the books_genres table in section 14 first
 
 -- Delete an author here:
